dd20df2cebdb6cf13c64adfd7d0aeabd1eb1fa14
Tegadaniel <tegadaniels8@gmail.com>
2023-01-15 06:42:01 +0100

    Modal popup change

--- src/components/modals/modalPopUpSmall.jsx
+++ src/components/modals/modalPopUpSmall.jsx
@@ -1,30 +1,25 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import { makeStyles } from '@material-ui/core/styles';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(() => ({
   backDrop: {
-    backdropFilter: 'blur(3px)',
-    backgroundColor: 'rgba(0,0,30,0.4)',
+    backdropFilter: "blur(3px)",
+    backgroundColor: "rgba(0,0,30,0.4)",
   },
 }));
 
-export default function ModalPopUpSmall({
-  open,
-  handleClose,
-  children,
-  width,
-}) {
+export default function ModalPopUpSmall({ open, handleClose, children }) {
   const inlineStyle = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: width || '400px',
-    padding: '25px, 5px',
-    borderRadius: '10px',
-    bgcolor: 'background.paper',
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "400px",
+    padding: "25px, 5px",
+    borderRadius: "10px",
+    bgcolor: "background.paper",
     boxShadow: 24,
     p: 4,
   };
